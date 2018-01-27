50a9dde55a32b32244cce1e72e33285de9fd13bc
philvoyer <philippe.voyer@ift.ulaval.ca>
2018-01-27 16:33:30 -0500

    mise à jour exemple 1.6
    déplacement d'une étapte et commentaires

--- module01/EX06/IFT3100H18_GL_ImageExport.cpp
+++ module01/EX06/IFT3100H18_GL_ImageExport.cpp
@@ -2,7 +2,7 @@
 // Exemple d'une section de code pour exporter une image à partir du contenu d'un framebuffer avec OpenGL.
 
 // entrée
-// • L'identifiant unique du framebuffer à copier.
+// • Identifiant du framebuffer à copier.
 // sortie
 // • Un fichier image qui contient une copie du contenu du framebuffer.
 
@@ -14,8 +14,7 @@ int image_component;
 int image_pixel_count;
 int image_size;
 
-// déclarer un pointeur vers un espace mémoire qui servira à stocker les pixels de l'image
-GLubyte* pixels;
+// 1. Extraction des propriétés du framebuffer
 
 // sélectionner le framebuffer à échantillonner
 glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
@@ -27,23 +26,33 @@ glGetRenderbufferParameteriv(GL_RENDERBUFFER, GL_RENDERBUFFER_HEIGHT, &image_hei
 // déterminer le nombre de composantes de couleur selon le format de l'image
 image_component = 4; // 4 composantes de couleur pour du RGBA
 
-// calculer le nombre total de pixels de l'image
+// 2. allocation de l'espace mémoire pour les pixels de l'image
+
+// calculer le nombre total de pixels dans l'image
 image_pixel_count = image_width * image_height;
 
-// calculer le nombre total d'octets à allouer en mémoire pour contenir l'image
-// (largeur x hauteur x nombre de composantes de couleurs)
+// calculer le nombre d'octets à allouer en mémoire pour contenir l'image
 image_size = image_pixel_count * image_component;
 
-// allocation d'un espace mémoire pouvant contenir tous les pixels de l'image
+// déclarer un pointeur vers un espace mémoire qui servira à stocker les pixels de l'image
+GLubyte* pixels;
+
+// allocation d'un espace mémoire suffisamment grand pour contenir tous les pixels de l'image
 pixels = (GLubyte*) calloc(image_size, sizeof(GLubyte));
+
+
+// 3. copier le contenu du framebuffer
 
 // lire le contenu du framebuffer actif et copier les pixels en mémoire RAM
 glReadPixels(0, 0, image_width, image_height, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
 
+
+// 4. sauvegarde dans un fichier image
+
 // ouvrir un nouveau fichier image en mode écriture
 // ...
 
-// écrire le contenu de l'image de la mémoire (RAM) vers le fichier
+// écrire les données de l'image dans le fichier image
 // ...
 
 // fermer le fichier image
