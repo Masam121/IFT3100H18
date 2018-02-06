1c7d0a2d74dcc63bc54a505a120aecfb2c247bd4
philvoyer <philippe.voyer@ift.ulaval.ca>
2018-02-06 10:09:26 -0500

    publication exemple 2.2
    DrawCursor

---
+++ module02/EX02/IFT3100H18_DrawCursor/src/main.cpp
@@ -0,0 +1,11 @@
+// IFT3100H18_DrawCursor/main.cpp
+// Dessine un curseur en fonction des états et des événements d'un périphérique de pointage et affiche ces informations dans la console.
+
+#include "ofMain.h"
+#include "application.h"
+
+int main( )
+{
+  ofSetupOpenGL(512, 512, OF_WINDOW);
+  ofRunApp(new Application());
+}
